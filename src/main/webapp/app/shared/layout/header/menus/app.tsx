0ee491549fc0377a656759c63e878d97bc6672e8
Pawel Hadam <pawel.hadam19@gmail.com>
2018-09-23 17:22:25 +0200

    Change app dropdown order.

--- src/main/webapp/app/shared/layout/header/menus/app.tsx
+++ src/main/webapp/app/shared/layout/header/menus/app.tsx
@@ -8,17 +8,17 @@ import { NavDropdown } from '../header-components';
 export const AppMenu = props => (
   // tslint:disable-next-line:jsx-self-close
   <NavDropdown icon="chalkboard-teacher" name={translate('global.menu.app.main')} id="entity-menu">
-    <DropdownItem tag={Link} to="/flashcards/card">
-      <FontAwesomeIcon icon="asterisk" />&nbsp;<Translate contentKey="global.menu.app.card" />
-    </DropdownItem>
-    <DropdownItem tag={Link} to="/flashcards/category">
-      <FontAwesomeIcon icon="asterisk" />&nbsp;<Translate contentKey="global.menu.app.category" />
-    </DropdownItem>
     <DropdownItem tag={Link} to="/flashcards/preplayroom">
       <FontAwesomeIcon icon="asterisk" />&nbsp;<Translate contentKey="global.menu.app.playroom" />
     </DropdownItem>
     <DropdownItem tag={Link} to="/flashcards/statistics">
       <FontAwesomeIcon icon="asterisk" />&nbsp;<Translate contentKey="global.menu.app.statistics" />
+    </DropdownItem>
+    <DropdownItem tag={Link} to="/flashcards/card">
+      <FontAwesomeIcon icon="asterisk" />&nbsp;<Translate contentKey="global.menu.app.card" />
+    </DropdownItem>
+    <DropdownItem tag={Link} to="/flashcards/category">
+      <FontAwesomeIcon icon="asterisk" />&nbsp;<Translate contentKey="global.menu.app.category" />
     </DropdownItem>
   </NavDropdown>
 );
